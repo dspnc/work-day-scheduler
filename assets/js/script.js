f32210076558314d62ac60776be4de89e4db42db
dspnc <dspence.hill@gmail.com>
2023-02-26 18:29:09 -0500

    time blocks added to HTML, current Day added to top using dayjs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+var currentDay = dayjs().format("dddd, MMMM D YYYY");
+
+$("#currentDay").text(currentDay);
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
